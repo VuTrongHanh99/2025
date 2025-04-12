1fcafb06f59ada3bae9b771a6b566641192d4e3f
hanhvt <hanhvt@dtsoft.com.vn>
2025-04-12 17:47:50 +0700

    change

---
+++ 01.SignalR/Be.SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+namespace SignalR.Hubs
+{
+    public class SignalHub : Hub
+    {
+        public async Task SendSignal(string user, string signalData)
+        {
+            // Gửi message đến tất cả các client trừ người gửi
+            await Clients.Others.SendAsync("ReceiveSignal", user, signalData);
+        }
+    }
+}
---
+++ 01.SignalR/Be.SignalR/Program.cs
@@ -0,0 +1,49 @@
+using SignalR.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// Thêm SignalR vào DI
+builder.Services.AddSignalR();
+builder.Services.AddCors(options =>
+{
+    //options.AddDefaultPolicy(policy =>
+    //{
+    //    policy.AllowAnyHeader()
+    //          .AllowAnyMethod()
+    //          .AllowCredentials()
+    //          .SetIsOriginAllowed(origin => true); // cho phép t?t c? origin
+    //});
+    options.AddPolicy("AllowAngularClient", policy =>
+    {
+        policy.WithOrigins("http://localhost:4210") // Angular dev server
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials() // Cho phép dùng cookie/signalr websocket
+              .SetIsOriginAllowed(origin => true); // cho phép t?t c? origin
+    });
+});
+var app = builder.Build();
+// Use CORS policy
+app.UseCors("AllowAngularClient");
+//app.UseCors();
+app.MapHub<SignalHub>("/signalhub");
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
